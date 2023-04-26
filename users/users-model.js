3128bed679ea1eba70558c48180dea6bc3ead613
alex-w-99 <awilcox99@gmail.com>
2023-04-26 15:19:36 -0400

    completed delete user model

--- users/users-model.js
+++ users/users-model.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import usersSchema from "./users-schema.js";
 import followModel from "../follow/follow-model.js";
+import ratingsModel from "../restaurants/ratings/ratings-model.js";
+import reviewsModel from "../restaurants/reviews/reviews-model.js";
 
 // when a user is deleted, so too must we delete all associated information:
 usersSchema.post(
@@ -9,9 +11,8 @@ usersSchema.post(
         const uid = this.getQuery()._id;
         await followModel.deleteMany({ followee: uid });
         await followModel.deleteMany({ follower: uid });
-        //await followModel.deleteMany({followee: { $in: [uid] }, follower: { $in: [uid] } });
-        //await followModel.deleteMany({$or: [{followee: uid}, {follower: uid}]});
-
+        await ratingsModel.deleteMany({ userId: uid });
+        await reviewsModel.deleteMany({ userId: uid });
     }
 );
 
